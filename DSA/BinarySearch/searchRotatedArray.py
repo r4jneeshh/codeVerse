ce58579786a44c839e11155587e3fa86d7ec18e6
Rajneesh <rajneeshthakur26@gmail.com>
2024-05-13 23:56:48 +0530

    Added search in RotatedArray

---
+++ DSA/BinarySearch/searchRotatedArray.py
@@ -0,0 +1,44 @@
+def search(nums:list, target: int) -> int:
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+
+        mid = (right + left) // 2
+
+        # if middle value is the target then return the mid index
+        if nums[mid] == target:
+            return mid
+
+        # to check if the mid is in left sorted portion
+        if nums[mid] >= nums[left]:
+
+            # if target > mid then move to right side
+            if target > nums[mid]:
+                left = mid + 1
+
+            else:
+                # check if target is less than left then move left to mid + 1
+                # else right to mid - 1
+                if target < nums[left]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+        # now we are in right sorted portion
+        else:
+
+            # target is less than mid move right to mid - 1
+            if target < nums[mid]:
+                right = mid - 1
+
+            else:
+                # check if target is greater than the right then move right to mid - 1
+                if target > nums[right]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+    # if number is not found then return -1
+    return - 1
+
+print(search([3,4,5,6,1,2],2))
